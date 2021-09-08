a64b8df94a2b4b82bd5a8a9975b160191fe2b651
luizsilva <luizfsilva.091595@gmail.com>
2021-09-08 11:38:33 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
-import logo from './logo.svg';
 import './App.css';
+import Chuck from '../src/chucknorris.jpeg'
+import { useEffect, useState } from 'react';
+import axios from 'axios'
 
 function App() {
+
+  const [state, setState] = useState({
+    joke: ''
+  })
+
+  useEffect( () => {
+    fetchData()
+  }, [])
+
+  const fetchData = async () => {
+    const result = await axios.get('https://api.chucknorris.io/jokes/random')
+    console.log(result.data.value)
+    setState({
+      ...state,
+      joke: result.data.value
+    })
+    console.log('this is state-->', state)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div className="container">
+    <div className="row">
+      <div className="col-6">
+        <h1 className="title">Chuck Norris API</h1>
+        <img src={Chuck} alt="Chuck Norris" />
+      </div>
+
+    <div className="col-6 searchJokeCol">
+      <div className="card">
+        <div className="header">
+          <span>Search for a word</span>
+        </div>
+        <div className="card-body">
+          <input type="text" />
+        </div>
+      </div> 
+
+      <div>
+        <button className="btn btn-warning btn-lg">Generate Joke</button>
+      </div>
+
     </div>
+
+    </div>
+
+    <h2 className="subtitle"> Here is the joke</h2>
+    <h4>{state.joke}</h4>
+  </div>
   );
 }
 
